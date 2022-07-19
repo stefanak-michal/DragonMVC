32803cfa91f963892534efd5a0e15b00c05cfedb
Michal Stefanak <stefanak.michal@gmail.com>
2022-07-19 11:34:23 +0200

    Removed integrated Neo4j wrapper and separated it.

--- config/main.cfg.php
+++ config/main.cfg.php
@@ -17,3 +17,21 @@ $aConfig = array(
     ],
 
 );
+
+// Neo4j wrapper default configuration
+Neo4j::$auth = \Bolt\helpers\Auth::basic('neo4j', 'neo4j');
+
+if (IS_WORKSPACE) {
+    Neo4j::$logHandler = function (string $query, array $params = [], int $executionTime = 0, array $statistics = []) {
+        $st = '';
+        foreach (array_filter($statistics) as $key => $value) {
+            $st .= '<b>' . $key . ':</b> ' . $value . '<br>';
+        }
+
+        \core\Debug::query($query, array_slice(debug_backtrace(2), 2), [
+            'params' => !empty($params) ? '<pre>' . print_r($params, true) . '</pre>' : '',
+            'stats' => !empty($st) ? '<pre>' . $st . '</pre>' : '',
+            'time (ms)' => $executionTime
+        ]);
+    };
+}
